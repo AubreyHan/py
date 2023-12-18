0c7c60075133d46997e22d329397ab3074c49cd4
Aubrey Han <aubreyhan@live.com>
2023-12-18 02:57:38 +0000

    001

--- demo/code/temp/demo01.py
+++ demo/code/temp/demo01.py
@@ -1,8 +1,19 @@
-import copy
+import vertexai
+from vertexai.language_models import TextGenerationModel
 
-a = [1,2,3,[4]]
-b = copy.deepcopy(a)
-b[3].append(5)
-
-print (a)
-print (b)
+vertexai.init(project="hy-ai-demo", location="us-central1")
+parameters = {
+    "max_output_tokens": 1024,
+    "temperature": 0.2,
+    "top_p": 0.8,
+    "top_k": 40
+}
+model = TextGenerationModel.from_pretrained("text-bison-32k@002")
+responses = model.predict_streaming(
+    """请描述如何学习英语""",
+    **parameters
+)
+results = []
+for response in responses:
+  print(response)
+  results.append(str(response))
